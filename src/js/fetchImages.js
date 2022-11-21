0066c98430931235599c3f2aed106d4715e84adf
Tetiana Deineka <tata4ka666@i.ua>
2022-11-21 17:31:18 +0200

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,15 +1,9 @@
-export const fetchImages = async (inputValue, pageNum) => {
-    return await fetch(`https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNum}`)
-    .then(async response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          return [];
-        }
-        throw new Error(response.status);
-      }
-      return await response.json();
-    })
-    .catch(error => {
-      console.error(error);
-    });
+ import axios from 'axios';
+
+axios.defaults.baseURL =
+  'https://pixabay.com/api/?key=31491056-913106eba1c8b28fe3dc938e7';
+
+export const fetchImages = async (inputValue, pageNumber) => {
+  const { data } = await axios.get(`&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNumber}`);
+  return data;
 }
