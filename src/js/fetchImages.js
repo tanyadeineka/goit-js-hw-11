8b55e201027fac41e1568c8ed5216fa23672f84c
Tetiana Deineka <tata4ka666@i.ua>
2022-11-22 12:38:51 +0200

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,14 +1,15 @@
- import axios from 'axios';
+/* import axios from 'axios'; 
+export { fetchImages };
 
 axios.defaults.baseURL =
   'https://pixabay.com/api/?key=31491056-913106eba1c8b28fe3dc938e7';
 
-export const fetchImages = async (inputValue, pageNumber) => {
-  const { data } = await axios.get(`&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNumber}`);
-  return data;
-}
+async function fetchImages(inputValue, pageNumber) {
+  const response = await axios.get(`&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNumber}`);
+  return response;
+} */
 
-/* export const fetchImages = async (inputValue, pageNr) => {
+export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
     `https://pixabay.com/api/?key=31491056-913106eba1c8b28fe3dc938e7&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
@@ -24,4 +25,4 @@ export const fetchImages = async (inputValue, pageNumber) => {
     .catch(error => {
       console.error(error);
     });
-}; */
+};
